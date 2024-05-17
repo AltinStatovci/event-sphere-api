34c746048a05e408ee87e4ef93dd6d7cfc88e8ca
Blendi <blendibs2000@gmail.com>
2024-05-17 15:06:38 +0200

    Created Models

---
+++ EventSphere/EventSphere.Domain/Entities/Payments.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Domain.Entities
+{
+    public class Payments
+    {
+        public int PaymentId { get; set; }
+        public Users UserId { get; set; }
+        public Events EventId { get; set; }
+        public Tickets TicketId { get; set; }
+        public int Amount { get; set; } 
+        public string PaymentMethod { get; set; }
+        public bool PaymentStatus { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
