571025a27d6f7a1e86a44b988025c589033a37ec
Çlirim Krasniqi <drin.krasniqi1@outlook.com>
2024-05-20 16:46:22 +0200

    Added GenericRepository

---
+++ EventSphere/EventSphere.Infrastructure/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace EventSphere.Infrastructure.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> predicate);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(int id);
+    }
+}
