b615fc980573fadca2ee06e8ae1b4bc43b3a6715
Blendi <blendibs2000@gmail.com>
2024-05-21 11:45:34 +0200

    Ticket Crud

---
+++ EventSphere/EventSphere.API/ApplicationInjection.cs
@@ -0,0 +1,16 @@
+﻿using EventSphere.Business.Services;
+using EventSphere.Business.Services.Interfaces;
+using EventSphere.Domain.IRepositories;
+using EventSphere.Infrastructure.Repositories;
+
+namespace EventSphere.API
+{
+    public static class ApplicationInjection
+    {
+        public static void AddEventSphereServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITicketRepository, TicketRepository>();
+            services.AddScoped<ITicketServices, TicketServices>();
+        }
+    }
+}
---
+++ EventSphere/EventSphere.Domain/IRepositories/ITicketRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Domain.IRepositories
+{
+    public interface ITicketRepository
+    {
+        Task<List<Ticket>> GetAllAsync();
+        Task<Ticket>GetByIdAsync(int id);
+        Task AddAsync(Ticket Tid);
+        Task UdpateAsync(Ticket Tid);
+        Task DeleteAsync(int id);
+    }
+}
