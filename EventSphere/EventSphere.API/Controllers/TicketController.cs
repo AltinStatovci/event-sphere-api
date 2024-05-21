757a4e242c66d1dde2ee5e5eed519a0c83616fb5
Blendi <blendibs2000@gmail.com>
2024-05-21 11:45:34 +0200

    Ticket Crud

---
+++ EventSphere/EventSphere.API/Controllers/TicketController.cs
@@ -0,0 +1,60 @@
+﻿using EventSphere.Business.Services.Interfaces;
+using EventSphere.Domain.DTOs;
+using EventSphere.Domain.Entities;
+using EventSphere.Domain.IRepositories;
+using EventSphere.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSphere.API.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly ITicketServices _ticketServices;
+        public TicketController(ITicketServices ticketServices)
+        {
+            _ticketServices = ticketServices;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Ticket>>> GetTicketsAsync()
+        {
+            await _ticketServices.GetAllTicketsAsync();
+            return Ok();
+        }
+        [HttpPost ("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicketAsync(int id)
+        {
+            var ticket = await _ticketServices.GetTicketByIdAsync(id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+            return Ok(ticket);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Ticket>> CreateAsync([FromBody] TicketDTO ticketDto)
+        {
+            if(ticketDto == null)
+            {
+                return BadRequest();
+            }
+            var ticket = await _ticketServices.AddTicketAsync(ticketDto);
+            return CreatedAtAction(nameof(GetTicketAsync), new { id = ticket.ID }, ticket);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Ticket>> EditAsync(int id, [FromBody] TicketDTO ticketDto)
+        {
+            if(id == 0 || ticketDto == null)
+            {
+                return BadRequest();
+            }
+            await _ticketServices.UdpateTicketAsync(id, ticketDto);
+            return NoContent();
+        }
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            await _ticketServices.DeleteTicketAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ EventSphere/EventSphere.Business/Services/TicketServices.cs
@@ -0,0 +1,66 @@
+﻿using EventSphere.Business.Services.Interfaces;
+using EventSphere.Domain.DTOs;
+using EventSphere.Domain.Entities;
+using EventSphere.Domain.IRepositories;
+using EventSphere.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Business.Services
+{
+    public class TicketServices : ITicketServices
+    {
+        private readonly ITicketRepository _ticketRepository;
+        public TicketServices(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+        public async Task<Ticket> AddTicketAsync(TicketDTO Tid)
+        {
+            var ticket = new Ticket
+            {
+                ID = Tid.ID,
+                EventID = Tid.EventID,
+                UserID = Tid.UserID,
+                TicketType = Tid.TicketType,
+                Price = Tid.Price,
+                DatePurchased = Tid.DatePurchased
+
+            };
+            await _ticketRepository.AddAsync(ticket);
+            return ticket;
+            
+        }
+
+        public async Task DeleteTicketAsync(int id)
+        {
+            await _ticketRepository.DeleteAsync(id);
+        }
+
+        public async Task<List<Ticket>> GetAllTicketsAsync()
+        {
+            return await _ticketRepository.GetAllAsync();
+        }
+
+        public async Task<Ticket> GetTicketByIdAsync(int id)
+        {
+            return await _ticketRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Ticket> UdpateTicketAsync(int id, TicketDTO Tid)
+        {
+            var tick = await _ticketRepository.GetByIdAsync(id);
+            tick.ID = Tid.ID;
+            tick.EventID = Tid.EventID;
+            tick.UserID = Tid.UserID;
+            tick.TicketType = Tid.TicketType;
+            tick.Price = Tid.Price;
+            tick.DatePurchased = Tid.DatePurchased;
+            await _ticketRepository.UdpateAsync(tick);
+            return tick;
+        }
+    }
+}
