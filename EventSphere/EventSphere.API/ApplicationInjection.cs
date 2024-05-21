86709a9695b8db641e06142b0a7ead75468095de
Blendi <blendibs2000@gmail.com>
2024-05-21 16:24:55 +0200

    fix

--- EventSphere/EventSphere.API/ApplicationInjection.cs
+++ EventSphere/EventSphere.API/ApplicationInjection.cs
@@ -1,6 +1,6 @@
 ﻿using EventSphere.Business.Services;
 using EventSphere.Business.Services.Interfaces;
-
+using EventSphere.Domain.Entities;
 using EventSphere.Infrastructure.Repositories;
 
 namespace EventSphere.API
@@ -10,6 +10,7 @@ namespace EventSphere.API
         public static void AddEventSphereServices(this IServiceCollection services)
         {
             services.AddScoped<ITicketServices, TicketServices>();
+            services.AddScoped<IGenericRepository<Ticket>, GenericRepository<Ticket>>();
         }
     }
 }
