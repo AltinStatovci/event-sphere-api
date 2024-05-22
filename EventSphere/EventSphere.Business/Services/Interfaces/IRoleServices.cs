88aed7ca1a7a62bb055c50c38771ddf5dd0c104f
Elsa <elsa.rrustemi@gmail.com>
2024-05-22 15:07:04 +0200

    Adding CRUD for Roles

---
+++ EventSphere/EventSphere.Business/Services/Interfaces/IRoleServices.cs
@@ -0,0 +1,19 @@
+﻿using EventSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Business.Services.Interfaces
+{
+    public interface IRoleServices
+    {
+        Task<IEnumerable<Role>> GetAllRolesAsync();
+        Task<Role> GetRoleByIdAsync(int roleId);
+        Task<Role> AddRoleAsync(Role role);
+        Task UpdateRoleAsync(Role role);
+        Task DeleteRoleAsync(int roleId);
+
+    }
+}
---
+++ EventSphere/EventSphere.Business/Services/RoleService.cs
@@ -0,0 +1,45 @@
+﻿using EventSphere.Business.Services.Interfaces;
+using EventSphere.Domain.Entities;
+using EventSphere.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Business.Services
+{
+    public class RoleServices : IRoleServices
+    {
+        private readonly IGenericRepository<Role> _genericRepository;
+
+        public RoleServices(IGenericRepository<Role> genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+        public async Task<Role> AddRoleAsync(Role role)
+        {
+            return await _genericRepository.AddAsync(role);
+        }
+
+        public async Task DeleteRoleAsync(int roleId)
+        {
+            await _genericRepository.DeleteAsync(roleId);
+        }
+
+        public async Task<IEnumerable<Role>> GetAllRolesAsync()
+        {
+            return await _genericRepository.GetAllAsync();
+        }
+
+        public async Task<Role> GetRoleByIdAsync(int roleId)
+        {
+            return await _genericRepository.GetByIdAsync(roleId);
+        }
+
+        public async Task UpdateRoleAsync(Role role)
+        {
+            await _genericRepository.UpdateAsync(role);
+        }
+    }
+}
