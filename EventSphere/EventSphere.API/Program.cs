69db3486337df634daf60a85c331ba21ef85a6a8
Çlirim Krasniqi <drin.krasniqi1@outlook.com>
2024-05-22 14:57:17 +0200

    Resolve conflicts

--- EventSphere/EventSphere.API/Program.cs
+++ EventSphere/EventSphere.API/Program.cs
@@ -1,12 +1,5 @@
 using EventSphere.API;
-using EventSphere.Business.Helper;
-using EventSphere.Business.Services;
-using EventSphere.Business.Services.Interfaces;
 using EventSphere.Infrastructure;
-using EventSphere.Infrastructure.Repositories;
-using EventSphere.Infrastructure.Repositories.UserRepository;
-using Mapster;
-using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,14 +14,6 @@ builder.Services.AddEventSphereServices(builder.Configuration);  // Pass the con
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-
-var config = TypeAdapterConfig.GlobalSettings;
-config.Scan(typeof(Program).Assembly);
-builder.Services.AddSingleton(config);
-builder.Services.AddScoped<IMapper, ServiceMapper>();
 
 var app = builder.Build();
 
