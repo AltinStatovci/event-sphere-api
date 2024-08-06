65c96af967806f89627a92a05ae250475182811f
lizableta <bletaeliza@gmail.com>
2024-08-06 15:00:25 +0200

    feat: Added Promo Code Crud

---
+++ EventSphere/EventSphere.Business/Services/Interfaces/IPromoCodeService.cs
@@ -0,0 +1,20 @@
+﻿using EventSphere.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventSphere.Business.Services.Interfaces
+{
+    public interface IPromoCodeService
+    {
+        Task<IEnumerable<PromoCode>> GetAllPromoCodesAsync();
+        Task<PromoCode> GetPromoCodeByIdAsync(int id);
+        Task<PromoCode> CreatePromoCodeAsync(PromoCode promoCode);
+        Task UpdatePromoCodeAsync(PromoCode promoCode);
+        Task DeletePromoCodeAsync(int id);
+        Task<PromoCode> GetByCodeAsync(string code);
+        Task<bool> IsPromoCodeValidAsync(string code);
+    }
+}
